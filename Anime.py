055617e14e2aa4636884cb16c848fd07307a24c8
Makaveli <josertrs@gmail.com>
2020-08-21 19:33:08 -0700

    Query page of current season anime

--- Anime.py
+++ Anime.py
@@ -1,8 +1,51 @@
 
+import requests
+
+
+
+
 class AnimeScheduler:
 
 	promptList = ["Print current Anime Shows", "View saved remiders", "Add a new reminder", "Delete a reminder", "Done"]
-	
+	currentAnime = []
+
+	url = "https://graphql.anilist.co"
+	query = '''
+	query ($page: Int, $perPage: Int) {
+		Page (page: $page, perPage: $perPage) {
+			pageInfo {
+				total
+				currentPage
+				lastPage
+				hasNextPage
+				perPage
+			}
+		
+			media (season: SUMMER, seasonYear: 2020, type: ANIME){
+				id
+				title {
+					romaji
+					native
+					english
+				}
+			}
+		}
+	}
+	'''
+
+	variables = {
+		'page': 1,
+		'perPage': 10
+	}
+	#__init__(self):
+		#initialize current anime list
+
+	def printCurrentAnime(self):
+		#check to see if I have cached list; else request new list
+		response = requests.post(self.url, json={'query': self.query, 'variables': self.variables})
+		pasteBin = response.text
+		print("All of the shit is: %s"%pasteBin)
+
 	def printPrompt(self):
 
 		while True:
@@ -11,7 +54,16 @@ class AnimeScheduler:
 				print(idx, ") ", prompt)
 			inp = input("Enter a option: ")
 			#do input command here
-			if inp == '4':
+			if inp == '0':
+				self.printCurrentAnime()
+			elif inp == '1':
+				self.printReminders()
+			elif inp == '2':
+				self.addReminder()
+			elif inp == '3':
+				self.deleteReminder()
+			elif inp == '4':
 				break
-
+			else:
+				print("Invalid Input; please try again")
 
